2a23fca86c250edc79e260028efcd09e5004b4cc
Nalle <nalle.juslen@gmail.com>
2023-11-01 07:19:30 +0200

    Update deltest.sh
    Added the new set for 
    - myTemplateBuilder v4 and 
    - K8s-Builder
    - K3s-Builder

--- deltest.sh
+++ deltest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
-#deltest.sh 1.0
+# deltest.sh 1.1
 
+qm destroy 8000 --destroy-unreferenced-disks 1 --purge 1 --skiplock 0
 qm destroy 9000 --destroy-unreferenced-disks 1 --purge 1 --skiplock 0
+qm destroy 5000 --destroy-unreferenced-disks 1 --purge 1 --skiplock 0
 qm destroy 5001 --destroy-unreferenced-disks 1 --purge 1 --skiplock 0
 qm destroy 5002 --destroy-unreferenced-disks 1 --purge 1 --skiplock 0
 qm destroy 5003 --destroy-unreferenced-disks 1 --purge 1 --skiplock 0
