8612dbabfa795bae0b2f64b45f7b363b844244cd
Nalle <nalle.juslen@gmail.com>
2023-11-29 15:32:24 +0200

    Create PveInit.sh
    A personal touch on Pve

---
+++ scripts/PveInit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+clear
+echo "This script will add my personal preferenses to Proxmox nodes"
+echo ""
+echo "  Adding apps to your system:"
+echo "  - bat, cat on steroids"
+echo "  - exa, ls as a modern app"
+echo "  - nala, apt with a modern look"
+echo "  
+# Add bat exa nala
+apt update && apt install -y bat exa nala >>install.log 2>&1
+echo "  Adding aliases to your bash and a new prompt"
+# Add to or change the bash commands  1>>$ok_log 2>>$ei_log
+wget https://github.com/nallej/MyJourney/raw/main/.bash_aliases >>wget.log 2>&1
+# And also the personal bash prompt 
+wget https://github.com/nallej/MyJourney/raw/main/.bash_prompt >>wget.log 2>&1
+# Activate the changes
+echo ""
+echo ""
+echo "  Adding bash_aliases to bashrc"
+echo "[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases" >> .bashrc
+echo ""
+echo "  Edit your new bash by nano .bash_aliases and make it yours"
+echo "    - change the IP mask if not /24 in [alias myip]"
+echo "    - add any alias you like"
+echo "    - comment out what you do not like"
+echo "    - exit from bat type q"
+sleep 1
+echo ""
+echo "To start using the new bash, type: . .bash_aliases (note the periods)"
