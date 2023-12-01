65d64a16094ff07be7ad752a592836193d293178
Nalle <nalle.juslen@gmail.com>
2023-12-01 13:50:30 +0200

    Update initVM.sh

--- scripts/initVM.sh
+++ scripts/initVM.sh
@@ -2,14 +2,13 @@
 # Download: wget https://github.com/nallej/MyJourney/raw/main/scripts/initVM.sh
 clear
 # echo "This script will add my personal preferenses to Proxmox nodes"
-# Add bat exa nala
-# apt update && apt install -y bat exa nala >>install.log 2>&1
+# Add bat, exa and nala
+# apt update && apt install -y bat exa nala
 # Adding aliases to your bash and a new prompt"
-wget https://github.com/nallej/MyJourney/raw/main/.bash_aliases >>wget.log 2>&1
-wget https://github.com/nallej/MyJourney/raw/main/.bash_prompt >>wget.log 2>&1
+wget https://github.com/nallej/MyJourney/raw/main/.bash_aliases
+#wget https://github.com/nallej/MyJourney/raw/main/.bash_prompt
 # Activate the changes
 # - Adding bash_aliases to bashrc"
-echo "[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases" >> .bashrc
-# sleep .5
+#echo "[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases" >> .bashrc
 # echo "To start using the new bash, type: . .bash_aliases (note the periods)"
 . .bash_aliases
