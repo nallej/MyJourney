a2d68a5be7a0db45f3331e472eddc4a54f628fba
Nalle <nalle.juslen@gmail.com>
2023-12-07 17:15:43 +0200

    Update K0s-starter.sh

--- scripts/K0s-starter.sh
+++ scripts/K0s-starter.sh
@@ -1,25 +1,24 @@
 # Start setup os K0s Controller/Worker
 clear
-echo -e "\nStart the K0s cluster\n"
-read -rp "IP worker 1: " IPworker1
-read -rp "IP worker 2: " IPworker2
-echo -e "\nAfter starting Controller and worker check status "
-echo "  - sudo k0s kubectl get nodes"
-
-
-if (whiptail --backtitle "TemplateBuilder"  --title "Start K0s Cluster" --defaultno --yesno \
-    "\nStart the K0s cluster." \
-    8 58 --no-button "worker" --yes-button "ctrlr"); then
-    #yes Controller
+echo -e "/nInitialize the K0s Cluster\n"
+read -rp "Init the Controller [y/N] " x
+if [[ $x == "y" ]]; then
+    #yes a Controller
+    sudo k0s start
     sudo k0s token create --role=worker > k0s.token
+    read -rp "IP worker 1: " IPworker1
+    read -rp "IP worker 2: " IPworker2
     sudo scp k0s.token $user@$IPworker1
     sudo scp k0s.token $user@$IPworker2
-    sudo k0s install controller --enable-worker
+    sudo k0s install controller --enable-worker # or standalone
     sudo k0s start
     sudo k0s status
     sudo k0s kubectl get nodes
+    echo -e "\nAfter starting the Controller and all worker check status "
+    echo "  - sudo k0s kubectl get nodes"
 else
     # Worker
     sudo k0s install worker --token-file k0s.token
     sudo k0s start
+    sudo k0s status
 fi
