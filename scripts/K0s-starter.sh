edbad1a0d89f751832f95121910aae8c6ca77522
Nalle <nalle.juslen@gmail.com>
2023-12-08 10:22:07 +0200

    Update K0s-starter.sh

--- scripts/K0s-starter.sh
+++ scripts/K0s-starter.sh
@@ -1,24 +1,26 @@
 # Start setup os K0s Controller/Worker
 clear
-echo -e "/nInitialize the K0s Cluster\n"
+echo ""
+echo "Initialize the K0s Cluster"
+echo ""
 read -rp "Init the Controller [y/N] " x
-if [[ $x == "y" ]]; then
+if [ "$x" = "y" ]; then
     #yes a Controller
-    sudo k0s start
+    sudo k0s install controller --enable-worker # or standalone
     sudo k0s token create --role=worker > k0s.token
     read -rp "IP worker 1: " IPworker1
     read -rp "IP worker 2: " IPworker2
     sudo scp k0s.token $user@$IPworker1
     sudo scp k0s.token $user@$IPworker2
-    sudo k0s install controller --enable-worker # or standalone
     sudo k0s start
     sudo k0s status
     sudo k0s kubectl get nodes
-    echo -e "\nAfter starting the Controller and all worker check status "
-    echo "  - sudo k0s kubectl get nodes"
 else
-    # Worker
+    # no on of the Workers
     sudo k0s install worker --token-file k0s.token
     sudo k0s start
     sudo k0s status
 fi
+echo ""
+echo "After starting the Controller and all worker check status "
+echo "  - sudo k0s kubectl get nodes"
