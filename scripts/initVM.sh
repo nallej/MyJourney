94a9c7432ecc8058d7c01747186f64b04ed94900
Nalle <nalle.juslen@gmail.com>
2023-12-16 09:59:48 +0200

    Update initVM.sh

--- scripts/initVM.sh
+++ scripts/initVM.sh
@@ -11,11 +11,9 @@ echo "Adding aliases to your bash and a new prompt"
 wget https://github.com/nallej/MyJourney/raw/main/.bash_aliases -O .bash-personal
 wget https://github.com/nallej/MyJourney/raw/main/.bash_prompt
 echo "Activating the changes"
-#  - Adding bash_aliases to bashrc and activate"
+#  - Adding bash_aliases to .bashrc"
 # echo "[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases" >> .bashrc
-# echo "To start using the new bash, type: . .bash_aliases (note the periods)"
-#. .bash_aliases
 echo "  - Adding bash-personal to bashrc and activate"
 echo "[[ -f ~/.bash-personal ]] && . ~/.bash-personal" >> .bashrc
-# echo "To start using the new bash, type: . .bash-personal (note the periods)"
-. .bash-personal
+echo "Start using the new bash, type: . .bashrc (note the periods)"
+. .bashrc
