273fc731f206f3bad74d94612895c6212ed7c069
Nalle <nalle.juslen@gmail.com>
2024-02-21 15:40:33 +0200

    Update DockerDockge.sh

--- scripts/DockerDockge.sh
+++ scripts/DockerDockge.sh
@@ -2,9 +2,12 @@
 # Create a VM running Docker & Dockge for our stacks
 # Run as normal user in ~/
 
+# Make the directory for oure stacks
+mkdir -p /home/$USER/docker/
+# refresh and upgrade
 sudo apt update && sudo apt upgrade -y
 # Prerequisits
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 # GPG docker-archive-keyring
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # Docker repository
@@ -13,8 +16,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt update
 apt-cache policy docker-ce
 # Install Docker-ce, community edition and not the licenced one
-sudo apt install docker-ce -y
-# Make the directory for oure stacks
-mkdir -p /home/$USER/docker/
+sudo apt install -y docker-ce
+
 # Add Dockge container
 sudo docker run -d -p 5001:5001 --name Dockge --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/docker/dockge/data:/app/data -v /home/$USER/docker/stacks:/home/$USER/docker/stacks -e DOCKGE_STACKS_DIR=/home/$USER/docker/stacks louislam/dockge:latest
