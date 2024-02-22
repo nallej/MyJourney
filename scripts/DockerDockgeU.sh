b8aca1d007905bebd553396814e89627d1b6977d
Nalle <nalle.juslen@gmail.com>
2024-02-22 14:36:18 +0200

    Update and rename DockerDockge.sh to DockerDockgeU.sh

--- scripts/DockerDockgeU.sh
+++ scripts/DockerDockgeU.sh
@@ -12,7 +12,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # Docker repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-# Hous keeping
+# Houskeeping
 sudo apt update
 apt-cache policy docker-ce
 # Install Docker-ce, community edition and not the licenced one
