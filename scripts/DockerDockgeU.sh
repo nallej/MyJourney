37ca64fd36066cdf752f1ffd02fdac7cf4b2a0e4
Nalle <nalle.juslen@gmail.com>
2024-02-22 14:46:15 +0200

    Update DockerDockgeU.sh
    Added some extra and security issues

--- scripts/DockerDockgeU.sh
+++ scripts/DockerDockgeU.sh
@@ -20,3 +20,15 @@ sudo apt install -y docker-ce
 
 # Add Dockge container
 sudo docker run -d -p 5001:5001 --name Dockge --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/docker/dockge/data:/app/data -v /home/$USER/docker/stacks:/home/$USER/docker/stacks -e DOCKGE_STACKS_DIR=/home/$USER/docker/stacks louislam/dockge:latest
+
+# extra stuff
+#systemctl start docker.service
+#systemctl status docker.service
+#sleep 5
+#systemctl enable docker.service
+
+# While you can run Docker as a root user, doing so is discouraged,
+# because of potential security risks and accidental modifications to your Debian host system. 
+# Instead, manage Docker operations under a non-root user account to enhance security.
+#sudo useradd -m $USER -p userPASSWORD  #dockeruser, -m create home directory -p password
+sudo usermod -aG docker $USER           #dockeruser
