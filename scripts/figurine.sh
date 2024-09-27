41b984092a636b9e191a66797991ebe9b8316660
Nalle <nalle.juslen@gmail.com>
2024-09-27 09:29:02 +0300

    Update figurine.sh

--- scripts/figurine.sh
+++ scripts/figurine.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #scp figurine root@192.0.2.41:/usr/local/bin
 #scp figurine.sh root@192.0.2.41:/etc/profile.d/
+echo "" #starwars, Stampranello, Contessa Contrast, Mini, Shadow
+/usr/local/bin/figurine -f "starwars.flf" 10.10.110.43
 echo ""
-/usr/local/bin/figurine -f "3d.flf" Pve - 9
+echo ""
+/usr/local/bin/figurine -f "3d.flf" Pve - 3.lab
 echo ""
